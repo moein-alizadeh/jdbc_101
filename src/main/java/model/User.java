b37c3c381b908fffe68a6116312cd2bfe7e3a017
moein-alizadeh <moeinalizadeh293@gmail.com>
2023-10-03 11:35:05 +0330

    add needed attribute, constructor, getter and setter

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,5 +1,57 @@
 package model;
 
 public class User {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String passWord;
 
+    public User(Integer id, String firstName, String lastName, String userName, String passWord) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.passWord = passWord;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
 }
